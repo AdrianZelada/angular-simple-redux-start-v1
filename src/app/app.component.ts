8a1ed77af4e4c918dd2d67bc2e21aabb58c42249
AdrianZelada <zelada_torrez@hotmail.com>
2019-09-21 01:22:59 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+/*
+Objectives:
+1. must segregate state from actions from side effects
+2. should add numbers when click the button
+3. should subtract numbers when click the button
+4. should reset the counter when click the button
+5. should start the counter when click the button
+6. should pause the counter when click the button
+*/
+
+enum Action {
+  Start,
+  Pause,
+  Reset,
+  Add,
+  Subtract,
+}
+
+interface AppState {
+  count: number,
+}
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  private initialState: AppState = {
+    count: 0,
+  };
+
+  actionType = Action;
+  actions$ = new BehaviorSubject(Action.Reset);
+
+  state$: Observable<AppState> = of({...this.initialState});
+
+}
