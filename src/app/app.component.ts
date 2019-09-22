c35dea7836bd5e93ce43c61dba4142bee16c7038
AdrianZelada <zelada_torrez@hotmail.com>
2019-09-22 00:48:49 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { BehaviorSubject, Observable, of } from 'rxjs';
-import { map,scan } from 'rxjs/operators';
+import { BehaviorSubject, Observable,merge, defer,of, interval } from 'rxjs';
+import { map,scan, filter, switchMap, tap } from 'rxjs/operators';
 
 /*
 Objectives:
-1. must segregate state from actions from side effects
-2. should add numbers when click the button
-3. should subtract numbers when click the button
+1. must segregate state from actions from side effects x
+2. should add numbers when click the button x
+3. should subtract numbers when click the button x
 4. should reset the counter when click the button
 5. should start the counter when click the button
 6. should pause the counter when click the button
@@ -37,15 +37,32 @@ export class AppComponent  {
   actionType = Action;
   actions$ = new BehaviorSubject(Action.Reset);
 
-  // state$: Observable<AppState> = of({...this.initialState});
-  state$: Observable<AppState> = this.actions$.pipe(
+  state$: Observable<AppState> = merge(
+    this.actions$,
+    defer(() =>this.timeCtrl$)
+  ).pipe(
      scan((state: AppState, action: Action) => {
       switch (action) {
         case Action.Add:
           return { ...state, count: state.count + 1 };
+        case Action.Subtract:
+          return { ...state, count: state.count - 1 };
+        case Action.Reset:
+          return { ...state, ...this.initialState};
       }
       return state;
     }, {...this.initialState}),
   );
 
+  timeCtrl$ = this.actions$.pipe(
+    filter((action)=>{
+      return (action == Action.Start) || (action == Action.Pause)
+    }),
+    switchMap( (action) => interval(1000).pipe(
+      filter(() => action != Action.Pause)
+    )),
+    map(() => {
+      return Action.Add;
+    })
+  )
 }
